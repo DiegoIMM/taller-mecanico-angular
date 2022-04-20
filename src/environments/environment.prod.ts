06389932d33204f1086250db675ceb4dbe9d2d8a
Diego Martinez <diegomartinezpavez@icloud.com>
2022-04-20 00:07:47 -0400

    fix url con https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://204.48.31.201:8081/',
-  authUrl: 'http://204.48.31.201:8081/'
+  apiUrl: 'https://204.48.31.201:8081/',
+  authUrl: 'https://204.48.31.201:8081/'
 };
