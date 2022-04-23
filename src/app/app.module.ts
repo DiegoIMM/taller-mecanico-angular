3e6c087f6f0fd63ae83aef0ef4a21ce3001f32b7
Diego Martinez <diegomartinezpavez@icloud.com>
2022-04-23 12:30:52 -0400

    add hash to avoid 404 error on reload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 import {SharedModule} from './shared.module';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import {NotFoundComponent} from './pages/not-found/not-found.component';
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
